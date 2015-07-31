Fixed scaling errors

Data Processing/graphAbsoluteAcceleration.py
@@ -34,13 +34,14 @@ class DigitalPlot:
   # Add data to buffer array
   def add(self, data):
       assert(len(data) == 3)
-      self.addToBuf(self.ax, data[0]*0)
+      self.addToBuf(self.ax, 0)
 ##      self.addToBuf(self.ay, data[1]*.0312)
 ##      self.addToBuf(self.az, data[2]*.0312)
       x = data[0]*scale
       y = data[1]*scale
       z = data[2]*scale
       absoluteAcceleration = np.sqrt(x**(2)+y**(2)+z**(2))
+      print(x,y,z,absoluteAcceleration)
       self.addToBuf(self.abs, absoluteAcceleration)
 
   # update plot
@@ -88,9 +89,10 @@ def main():
 
   # set up animation
   fig = plt.figure()
-  ax = plt.axes(xlim=(0, 100), ylim=(-200, 500))
+  ax = plt.axes(xlim=(0, 100), ylim=(0,8))
   plt.title('Absolute Acceleration Stream')
   plt.ylabel('Absolute Acceleration (G)')
+  plt.grid(True)
   plt.xlabel('Data Point')
   a0, = ax.plot([], [])
 ##  a1, = ax.plot([], [])

Data Processing/graphSensorData.py
@@ -6,7 +6,7 @@ from collections import deque
 import matplotlib.pyplot as plt 
 import matplotlib.animation as animation
 
-    
+scale = .0078
 # Class to plot data
 class DigitalPlot:
   # constr
@@ -33,10 +33,15 @@ class DigitalPlot:
   # Add data to buffer array
   def add(self, data):
       assert(len(data) == 3)
-      self.addToBuf(self.ax, data[0]*.0312)
-      self.addToBuf(self.ay, data[1]*.0312)
-      self.addToBuf(self.az, data[2]*.0312)
-      self.addToBuf(self.abs, np.sqrt((data[0]*.0312)**(2)+(data[0]*.0312)**(2)+(data[0]*.0312)**(2)))
+      x = data[0]*scale
+      y = data[1]*scale
+      z = data[2]*scale
+      absolute = np.sqrt(x**(2)+y**(2)+z**(2))
+      self.addToBuf(self.ax, x)
+      self.addToBuf(self.ay, y)
+      self.addToBuf(self.az, z)
+      print( data[0]*scale, data[1]*scale, data[2]*scale)
+      self.addToBuf(self.abs, absolute)
 
   # update plot
   def update(self, frameNum, a0, a1, a2, a3):
@@ -83,7 +88,9 @@ def main():
 
   # set up animation
   fig = plt.figure()
-  ax = plt.axes(xlim=(0, 100), ylim=(-2000, 2000))
+  ax = plt.axes(xlim=(0, 100), ylim=(-8,8))
+  plt.title('ADXL345 Data')
+  plt.ylabel('Acceleration (G)')
   a0, = ax.plot([], [])
   a1, = ax.plot([], [])
   a2, = ax.plot([], [])